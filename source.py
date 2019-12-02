87ec9c635061dd533ef2047939a789c770aff7bf
Pranav Arora <pranav.arora@utexas.edu>
2019-12-02 16:36:57 -0600

    implement getBSSID()

--- source.py
+++ source.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 def setupNetworkReader(interface_name):
     print("setting up network card")
@@ -12,8 +13,18 @@ def setupNetworkReader(interface_name):
     except KeyboardInterrupt:
         print("failed to set up network card. check your interface name.")
 
+def getBSSID(essid, interface_name):
+	try:
+		clients = subprocess.Popen(["airodump-ng", interface_name)
+		for client in clients:
+			if essid in client:
+				return client.split()[0]
+		return None
+	except Exception:
+		print("Given ESSID not found.")
 
 if __name__ == "__main__":
     interface = input("Please enter the interface name of your wireless network reader. You can find this by running ifconfig. It should be a wlan.")
     monitorModeInterface = setupNetworkReader(interface)
-    
+	bssid = getBSSID(sys.argv[1])
+	print(bssid)    
