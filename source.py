859e71ee35c9988fcbc04269d884f55923fc6f34
sahilparikh98 <sparikh98@gmail.com>
2019-12-02 20:26:34 -0600

    unpushed changes

--- source.py
+++ source.py
@@ -13,7 +13,7 @@ def setupNetworkReader(interface_name):
     except KeyboardInterrupt:
         print("failed to set up network card. check your interface name.")
 
-def getBSSID(essid, interface_name):
+def getBSSIDInfo(essid, interface_name):
     print("press CTRL+C after your network pops up. we'll strip out the BSSID for you.\nIf your network doesn't pop up, we probably can't find it.")
     try: 
         cmd = "airodump-ng {} --essid {} -w allnet2".format(interface_name, essid)
@@ -24,7 +24,12 @@ def crackWEP(bssid, essid):
     print("cracking wep")
     # open up csv file with bssid in it, split by newline and then split by
     # run besside-ng
-    
+
+def connectToNetwork():
+    print("connecting to network")
+    # close airmon stuff
+    # start wireless card again
+    #   
 
 if __name__ == "__main__":
     interface = input("Please enter the interface name of your wireless network reader. You can find this by running ifconfig. It should be a wlan: ")
