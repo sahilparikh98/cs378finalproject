258cbfab9c62385d0a85e13b5f1313b7cc2575c0
sahilparikh98 <sparikh98@gmail.com>
2019-12-02 20:25:32 -0600

    added bssid code and stubs

--- source.py
+++ source.py
@@ -14,18 +14,22 @@ def setupNetworkReader(interface_name):
         print("failed to set up network card. check your interface name.")
 
 def getBSSID(essid, interface_name):
-	try:
-		clients = subprocess.run(["airodump-ng", interface_name], capture_output=True).stdout
-		for client in clients:
-			if essid in client:
-				return client.split()[0]
-		return None
-	except Exception:
-		print("Given ESSID not found.")
+    print("press CTRL+C after your network pops up. we'll strip out the BSSID for you.\nIf your network doesn't pop up, we probably can't find it.")
+    try: 
+        cmd = "airodump-ng {} --essid {} -w allnet2".format(interface_name, essid)
+    except KeyboardInterrupt:
+		print("obtained BSSID info")
+
+def crackWEP(bssid, essid):
+    print("cracking wep")
+    # open up csv file with bssid in it, split by newline and then split by
+    # run besside-ng
+    
 
 if __name__ == "__main__":
     interface = input("Please enter the interface name of your wireless network reader. You can find this by running ifconfig. It should be a wlan: ")
     monitorModeInterface = setupNetworkReader(interface)
     essid = input("Enter the ESSID (network name) of the access point you are trying to crack: ")
-    bssid = getBSSID(essid, monitorModeInterface)
+    getBSSIDInfo(essid, monitorModeInterface)
+    
     print(bssid)
